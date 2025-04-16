15740891dee99fc6fc6a7d9e8527092e5fec9f7f
Nurmdau <narymbaev.n02@gmail.com>
2025-04-16 20:27:30 +0500

    task 3 -> Redis connected & Implemented GET&POST requests

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"regexp"
+	"strconv"
 	"tasks_test_api/utils"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
 )
+
+var rdb *redis.Client
+var ctx = context.Background()
 
 func main() {
 	r := gin.Default()
 
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
 	r.POST("/rest/substr/find", findSubstringHandler)
 	r.POST("/rest/email/check", checkEmailHandler)
 	r.POST("/rest/iin/check", checkIINHandler)
+	r.POST("/rest/counter/add/:i", addCounterHandler)
+	r.POST("/rest/counter/sub/:i", subCounterHandler)
+	r.GET("/rest/counter/val", getCounterValueHandler)
 
 	r.Run(":8088")
 }
@@ -35,6 +50,7 @@ func checkEmailHandler(c *gin.Context) {
 	bodyBytes, err := c.GetRawData()
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "body text not found"})
+		return
 	}
 
 	bodyText := string(bodyBytes)
@@ -57,6 +73,7 @@ func checkIINHandler(c *gin.Context) {
 	bodyBytes, err := c.GetRawData()
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "could not parse raw data"})
+		return
 	}
 
 	bodyText := string(bodyBytes)
@@ -73,4 +90,59 @@ func checkIINHandler(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"iins": iins})
+}
+
+func addCounterHandler(c *gin.Context) {
+	param := c.Param("i")
+	val, err := strconv.ParseInt(param, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "could not parse param 'i'"})
+		return
+	}
+
+	res, err := rdb.IncrBy("counter", val).Result()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "could not increment"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "counter incremented, current value: " + strconv.FormatInt(res, 10)})
+}
+
+func subCounterHandler(c *gin.Context) {
+	param := c.Param("i")
+	val, err := strconv.ParseInt(param, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "could not parse param 'i'"})
+		return
+	}
+
+	res, err := rdb.DecrBy("counter", val).Result()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "could not decrement"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "counter decremented, current value: " + strconv.FormatInt(res, 10)})
+}
+
+func getCounterValueHandler(c *gin.Context) {
+	counterValue, err := rdb.Get("counter").Result()
+	if err != nil {
+		// Set to 0
+		err = rdb.Set("counter", 0, 0).Err()
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "could not get counter IN"})
+			return
+		}
+
+		// Get value
+		counterValue, err = rdb.Get("counter").Result()
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "could not get counter IN"})
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{"value": counterValue})
 }
