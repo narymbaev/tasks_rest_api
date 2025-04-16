c09f7ecf2f3e2ab4146d75f2af744e78c14f171f
Nurmdau <narymbaev.n02@gmail.com>
2025-04-16 18:58:46 +0500

    task 2

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+	"regexp"
+	"tasks_test_api/utils"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,6 +12,7 @@ func main() {
 	r := gin.Default()
 
 	r.POST("/rest/substr/find", findSubstringHandler)
+	r.POST("/rest/email/check", checkEmailHandler)
 
 	r.Run(":8088")
 }
@@ -23,6 +26,28 @@ func findSubstringHandler(c *gin.Context) {
 		return
 	}
 
-	maxSubstr := (req.Input)
+	maxSubstr := utils.LongestSubstring(req.Input)
 	c.JSON(http.StatusOK, gin.H{"result": maxSubstr})
+}
+
+func checkEmailHandler(c *gin.Context) {
+	bodyBytes, err := c.GetRawData()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "body text not found"})
+	}
+
+	bodyText := string(bodyBytes)
+
+	re := regexp.MustCompile(`(?i)Email:\s+([a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,})`)
+	matches := re.FindAllStringSubmatch(bodyText, -1)
+
+	emails := []string{}
+
+	for _, match := range matches {
+		if len(match) > 1 {
+			emails = append(emails, match[1])
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{"emails": emails})
 }
